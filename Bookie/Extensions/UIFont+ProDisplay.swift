5e7a9c18025529b0e097f83b86c05292ac6e14dd
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-05-29 23:09:11 +0300

    extensions

---
+++ Bookie/Extensions/UIFont+ProDisplay.swift
@@ -0,0 +1,20 @@
+//
+//  UIFont+ProDisplay.swift
+//  Bookie
+//
+//  Created by newAccount on 29.05.2023.
+//
+
+import Foundation
+import UIKit
+
+enum ProDisplayFontType: String {
+    case SFBold = "SFProDisplay-Bold"
+    case SFMedium = "SFProDisplay-Medium"
+}
+
+extension UIFont {
+    static func kanit(ofSize size: CGFloat, fontType type: ProDisplayFontType) -> UIFont {
+        return UIFont(name: type.rawValue, size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+}
