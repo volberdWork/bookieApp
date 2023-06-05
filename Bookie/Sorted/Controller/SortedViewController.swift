1cef0cc92f5fe607646d080bf3e80114733902e2
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-06-05 10:32:40 +0300

    fix ui for sortedScreen

---
+++ Bookie/Sorted/Controller/SortedViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SortedViewController.swift
+//  Bookie
+//
+//  Created by newAccount on 05.06.2023.
+//
+
+import UIKit
+
+class SortedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureView()
+    }
+    
+    private func configureView(){
+        view.backgroundColor = .customLightGrey
+    }
+
+}
