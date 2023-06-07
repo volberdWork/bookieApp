adddc901929602a2626fde32123379e3c8cd6e04
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-06-07 10:20:14 +0300

    lastFixes

---
+++ Bookie/Modules/MatchLive/Controller/MatchLiveViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MatchLiveViewController.swift
+//  Bookie
+//
+//  Created by newAccount on 07.06.2023.
+//
+
+import UIKit
+
+class MatchLiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
